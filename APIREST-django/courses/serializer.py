5e4192e7ed24970952f5ef7e48515ba8a729c210
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-05-20 00:49:54 -0300

    Relationship with DRF

--- APIREST-django/courses/serializer.py
+++ APIREST-django/courses/serializer.py
@@ -10,11 +10,34 @@ class AssessmentSerializer(serializers.ModelSerializer):
             'email': {'write_only': True}
         }
         model = Assessment
-        fields = '__all__'
+        fields = ('id',
+                  'course',
+                  'email',
+                  'comments',
+                  'assessment',
+                  'creates',
+                  'active'
+                  )
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    
+    """
+    Nested Relationship
+    assessments = AssessmentSerializer(many=True,read_only=True)
+    """
+
+    #HyperLinked Related Field
+    assessments = serializers.HyperlinkedRelatedField(
+                                                many=True, 
+                                                read_only=True, 
+                                                view_name="asssessment-detail")
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = (
+                  'id',
+                  'title',
+                  'url',
+                  'creates',
+                  'active',
+                'assessments',
+                )
